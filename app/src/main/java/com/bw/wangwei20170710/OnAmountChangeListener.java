1be9221f30d8ec00d6ab9ac00a56c976473fcd47
user <84716235@qq.com>
2017-07-10 10:10:01 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/wangwei20170710/OnAmountChangeListener.java
@@ -0,0 +1,11 @@
+package com.bw.wangwei20170710;
+
+import android.view.View;
+
+/**
+ * Created by user on 2017/7/10.
+ */
+
+interface OnAmountChangeListener {
+    void onAmountChange(View view, int amount);
+}
